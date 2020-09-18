fa3e6d39b4251bb6174e96f09d1a5a313141b2e4
unknown <Tmullen493@gmail.com>
2020-09-18 18:02:37 -0500

    Updates

---
+++ precache-manifest.e0f3c92ea476e0580aaa14ad18939a41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5adeb223bbbfe0159dd275389f34614",
+    "url": "/Year-You-Were-Born-React-App/index.html"
+  },
+  {
+    "revision": "170c2e506414a37f2b53",
+    "url": "/Year-You-Were-Born-React-App/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "5789a76868c0bb9f328b",
+    "url": "/Year-You-Were-Born-React-App/static/js/2.01de414c.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "/Year-You-Were-Born-React-App/static/js/2.01de414c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "170c2e506414a37f2b53",
+    "url": "/Year-You-Were-Born-React-App/static/js/main.692bb375.chunk.js"
+  },
+  {
+    "revision": "eb7fd0740ef98efc9865",
+    "url": "/Year-You-Were-Born-React-App/static/js/runtime-main.fa56e054.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Year-You-Were-Born-React-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
